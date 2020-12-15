108f7cf558fee94d95e552f77759f30d88c64369
Jan <s1120303@student.hsleiden.nl>
2020-12-15 13:10:06 +0100

    Relocated format function

--- helper/validateUser.js
+++ helper/validateUser.js
@@ -38,12 +38,3 @@ validateEmail = (email) => {
     }
     return false;
 }
-
-exports.formatName = (body) => {
-    body.firstname = body.firstname.toLowerCase().charAt(0).toUpperCase();
-    body.lastname = body.firstname.toLowerCase().charAt(0).toUpperCase();
-    if (body.middlename !== null) {
-        body.middlename = body.middlename.toLowerCase();
-    }
-    return body;
-}
