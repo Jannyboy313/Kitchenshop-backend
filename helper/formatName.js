2bf2a7ca85b610f1595dc57a2abdbf2345baf8c1
Jan <s1120303@student.hsleiden.nl>
2020-12-15 13:30:43 +0100

    Added formatting for email

--- helper/formatName.js
+++ helper/formatName.js
@@ -1,6 +1,7 @@
 exports.formatName = (body) => {
     body.firstname = body.firstname.toLowerCase().charAt(0).toUpperCase();
     body.lastname = body.firstname.toLowerCase().charAt(0).toUpperCase();
+    body.email = body.email.toLowerCase();
     if (body.middlename !== null) {
         body.middlename = body.middlename.toLowerCase();
     }
