1888990f1cc68f17fcdcbdee715c5203476c78a2
Jan <s1120303@student.hsleiden.nl>
2020-12-15 14:46:02 +0100

    Added format for address

--- helper/format.js
+++ helper/format.js
@@ -1,9 +1,22 @@
-exports.formatName = (body) => {
-    body.user.firstname = body.user.firstname.toLowerCase().charAt(0).toUpperCase();
-    body.user.lastname = body.user.firstname.toLowerCase().charAt(0).toUpperCase();
-    body.user.email = body.user.email.toLowerCase();
+exports.formatRegisterData = (body) => {
+    body = formatNameData(body);
+    body = formatAddressData(body);
+    return body;
+}
+
+formatNameData = (body) => {
+    body.user.firstname = body.user.firstname.toLowerCase().charAt(0).toUpperCase().trim();
+    body.user.lastname = body.user.firstname.toLowerCase().charAt(0).toUpperCase().trim();
+    body.user.email = body.user.email.toLowerCase().trim();
     if (body.user.middlename !== null) {
-        body.user.middlename = body.user.middlename.toLowerCase();
+        body.user.middlename = body.user.middlename.toLowerCase().trim();
     }
+    return body;
+}
+
+formatAddressData = (body) => {
+    body.address.city = body.address.city.toLowerCase().charAt(0).toUpperCase().trim();
+    body.address.street_address = body.address.street_address.toLowerCase().charAt(0).toUpperCase().trim();
+    body.address.zipcode = body.address.email.toUpperCase().split(" ").join("").trim();
     return body;
 }
