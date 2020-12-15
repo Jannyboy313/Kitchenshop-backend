5d6648f2e4e80d453ac6c0f11f2f601b13bd0983
Jan <s1120303@student.hsleiden.nl>
2020-12-15 13:30:25 +0100

    Added database validation

--- helper/validateUser.js
+++ helper/validateUser.js
@@ -1,17 +1,17 @@
 const regName = /^[a-z ,.'-]+$/i;
 const regEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const Users = require('../model/users.js');
 
-exports.validateUser = (body) => {
+exports.validateUser = async(body) => {
     if (!validateEmail(body.email)) {
         return false;
     }
     if (!validateName(body.firstname, body.lastname)) {
         return false
     }
-    if (!validateMiddlename(middlename)) {
+    if (!validateMiddlename(body.middlename)) {
         return false;
     }
-
     return true;
 }
 
@@ -33,8 +33,25 @@ validateMiddlename = (middlename) => {
 }
 
 validateEmail = (email) => {
-    if (regEmail.test(email)) {
+    if (regEmail.test(email) && validateExistence(email)) {
         return true;
     }
     return false;
+}
+
+validateExistence = async(email) => {
+    let existence;
+    try{
+        existence = await Users.findOne({
+            where: {
+                email: email,
+            }
+        })
+    } catch(err) {
+        return false;
+    }
+    if (existence === null) {
+        return false
+    }
+    return true;
 }
