4156cbcf3f3465aab85bd13fecda9472c6adb84e
Jan <s1120303@student.hsleiden.nl>
2020-12-15 09:17:24 +0100

    Created orders model

---
+++ model/orders.js
@@ -0,0 +1,31 @@
+import sequelize, { Sequelize } from '../db/connection.js';
+
+const Orders = sequelize.define('orders', {
+    orders_id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement : true
+    },
+    timestamp: {
+        type: Sequelize.DATE,
+        allowNull: false
+    },
+    customer_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+            model: customer,
+            key: 'customer_id'
+        }
+    },
+    articlenumber: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+            model: Product,
+            key: 'productnumber'
+        }
+    }
+});
+
+module.exports = (Orders);
