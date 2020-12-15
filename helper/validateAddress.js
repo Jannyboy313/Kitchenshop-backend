cfda24a553e04512333e6625fba06768a238a2b0
Jan <s1120303@student.hsleiden.nl>
2020-12-15 14:50:30 +0100

    Added validation for address

---
+++ helper/validateAddress.js
@@ -0,0 +1,11 @@
+const regex = /^[a-zA-Z0-9\s,'-]*$/
+
+exports.validateAddress = (address) => {
+    if (regex.test(address.city) &&
+        regex.test(address.street_address) &&
+        regex.test(address.zipcode)
+        ) {
+        return true;
+    }
+    return false;
+}
