4e05c7788b8987a280ea9d45c76674a524d8fa87
Jan <s1120303@student.hsleiden.nl>
2020-12-16 07:16:57 +0100

    Added router with routes

---
+++ routes/productRouter.js
@@ -0,0 +1,12 @@
+const productController = require("../controller/productController.js");
+const express = require('express');
+
+const router = express.Router();
+
+router.get("/product", productController.getProduct);
+// router.get("/products", productController.getProducts);
+// router.post("/addproduct", productController.addProduct);
+// router.put("/updateproduct", productController.updateProduct);
+// router.delete("/deleteproduct", productController.deleteProduct);
+
+module.exports = router;
