739111e2bec450af2d831cf7973bf6822a3c7a65
Jan <s1120303@student.hsleiden.nl>
2020-12-17 07:36:35 +0100

    Added new route

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get("/product", productController.getProduct);
 router.get("/products", productController.getProducts);
-// router.post("/addproduct", productController.addProduct);
+router.post("/addproduct", productController.addProduct);
 // router.put("/updateproduct", productController.updateProduct);
 // router.delete("/deleteproduct", productController.deleteProduct);
 
