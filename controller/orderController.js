0489bf05f0444ba92f4ba01bd6a6d43e148eb1c5
Jan <s1120303@student.hsleiden.nl>
2020-12-21 22:14:29 +0100

    Added functions for both roles

--- controller/orderController.js
+++ controller/orderController.js
@@ -1,22 +1,36 @@
 const Orders = require('../model/orders.js');
 
-exports.getOrder = async (req, res) => {
-    let orders_id = req.query.orders_id;
+exports.getCustomerOrders = async (req, res) => {
     let reply;
 
     try{
-        reply = await Orders.findOne({
-            where: {
-                orders_id: orders_id
-            }
-        })
+        reply = await Orders.findAll({where: {user_id: user_id} })
     } catch(err) {
         res.status(404).send(err.message);
         res.end();
         return;
     }
     if (!reply) {
-        res.status(404).send({"error": "Order not found"});
+        res.status(404).send({"error": "No orders exist"});
+        res.end()
+        return;
+    }
+    res.status(200).send(reply);
+    res.end()
+}
+
+exports.getAdminOrders = async (req, res) => {
+    let reply;
+
+    try{
+        reply = await Orders.findAll()
+    } catch(err) {
+        res.status(404).send(err.message);
+        res.end();
+        return;
+    }
+    if (!reply) {
+        res.status(404).send({"error": "No orders exist"});
         res.end()
         return;
     }
