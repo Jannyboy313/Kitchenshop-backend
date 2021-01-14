e11c2bb5073b5fb9ddec73899045f073c96f32a8
Jan <s1120303@student.hsleiden.nl>
2021-01-14 09:18:47 +0100

    Added deleteUser

--- controller/userController.js
+++ controller/userController.js
@@ -17,3 +17,19 @@ exports.getUsers = async (req, res) => {
     res.status(200).send(reply[0]);
     res.end();
 }
+
+exports.deleteUser = async (req, res) => {
+    const user_id = req.query.user_id;
+    try{
+        await Users.destroy({
+            where: {
+                user_id: user_id
+            }
+        })
+    } catch(err) {
+        res.status(404).send({"error": err});
+        console.log(err);
+    }
+    res.status(200);
+    res.end()
+}
